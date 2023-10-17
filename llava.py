f17e2bffbd3fab83971f105a85436a2dd742d742
CuiChi1109 <cuichi1109@gmail.com>
2023-10-17 16:52:19 +0800

    download checkpoint

---
+++ llava.py
@@ -0,0 +1,7 @@
+from transformers import AutoConfig, LlamaConfig
+from llava.model.language_model.llava_llama import LlavaLlamaForCausalLM
+class LlavaConfig(LlamaConfig):
+    model_type = "llava"
+AutoConfig.register("llava", LlavaConfig)
+model = LlavaLlamaForCausalLM.from_pretrained("liuhaotian/llava-llama-2-13b-chat-lightning-preview")
+print(model)
